29c081b1d5f873d0733497ff7f39dcd5d4ffa3b5
Grigorii Sharapov <socaseinpoint@gmail.com>
2019-10-18 13:01:07 +0600

    Trello Clone - Add redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
+/* eslint-disable */
+
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
 import { loadState, saveState } from '../utils/localStorage';
 
-const store = createStore(rootReducer, loadState());
+const store = createStore(
+  rootReducer, loadState(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 store.subscribe(() => {
   saveState(store.getState());
